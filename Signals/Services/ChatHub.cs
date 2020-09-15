4404c047ca35aa4b5d0f631860fb6f35f5ba0c5f
Osipova Daria <dasha.osd@gmail.com>
2020-09-15 21:19:31 +0300

    Skeleton for users

--- Signals/Services/ChatHub.cs
+++ Signals/Services/ChatHub.cs
@@ -8,9 +8,28 @@ namespace Signals.Services
 {
     public class ChatHub : Hub
     {
+        private Dictionary<string, string> _users = new Dictionary<string, string>();
+
         public async Task Send(string message)
         {
             await this.Clients.All.SendAsync("Send", message);
+        }
+
+        public override async Task OnConnectedAsync()
+        {
+            if (this._users.Keys.Contains(Context.ConnectionId))
+            {
+                return;
+            }
+
+            this._users[Context.ConnectionId] = "an0n #" + this._users.Count;
+            await this.Clients.All.SendAsync("NewUser", this._users[Context.ConnectionId]);
+            await base.OnConnectedAsync();
+        }
+        public override async Task OnDisconnectedAsync(Exception exception)
+        {
+            await this.Clients.All.SendAsync("UserLeft", this._users[Context.ConnectionId]);
+            await base.OnDisconnectedAsync(exception);
         }
     }
 }
--- Signals/wwwroot/js/client.js
+++ Signals/wwwroot/js/client.js
@@ -2,19 +2,6 @@
 
 const queryStringForInput = 'input[name="new_message"]';
 
-hubConnection.on("Send", (data) => {
-    let message = document.createElement("p");
-    message.classList.add("content_chat--content--message");
-    message.appendChild(document.createTextNode(data));
-    let firstElement = document.querySelector(".content_chat--content").firstChild;
-    document.querySelector(".content_chat--content").insertBefore(message, firstElement);
-});
-
-sendNewMessage = () => {
-    let message = document.querySelector(queryStringForInput).value;
-    hubConnection.invoke("Send", message);
-}
-
 document.querySelector(queryStringForInput).addEventListener("keyup", event => {
     const inputMessage = document.querySelector(queryStringForInput);
     if (inputMessage.value === null || inputMessage.value === undefined
@@ -27,5 +14,34 @@ document.querySelector(queryStringForInput).addEventListener("keyup", event => {
         document.querySelector(queryStringForInput).value = "";
     }
 });
+
+hubConnection.on("Send", data => {
+    let message = document.createElement("p");
+    message.classList.add("content_chat--content--message");
+    message.appendChild(document.createTextNode(data));
+    let firstElement = document.querySelector(".content_chat--content").firstChild;
+    document.querySelector(".content_chat--content").insertBefore(message, firstElement);
+});
+
+hubConnection.on("NewUser", data => {
+
+})
+
+hubConnection.on("UserLeft", data => {
+
+})
+
+const sendNewMessage = () => {
+    let message = document.querySelector(queryStringForInput).value;
+    hubConnection.invoke("Send", message);
+}
+
+const newUserEntered = () => {
+
+}
+
+const userLeft = () => {
+
+}
 
 hubConnection.start();
