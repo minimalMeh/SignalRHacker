616262da0f001c66f44d56ace46eae3a4517e9f1
minimalMeh <dasha.osd@gmail.com>
2020-09-23 13:28:21 -0700

    removed local storage

--- Signals/wwwroot/js/libs/SignalRServiceLib/js/UserService.js
+++ Signals/wwwroot/js/libs/SignalRServiceLib/js/UserService.js
@@ -1,44 +1,18 @@
 class UserService extends HubService {
     onUserEntered = (userName) => {
-        let users = sessionStorage.getItem("users");
-        if (users) {
-            users = Array.from(users);
-            if (users.includes(userName)) {
-                return;
-            }
-        }
-
         const userData = document.createElement("p");
         userData.classList.add("users_list--user");
         userData.appendChild(document.createTextNode(userName));
         const usersList = document.querySelector(".users_list");
         const lastElement = usersList.lastChild;
         usersList.insertBefore(userData, lastElement);
-        
-        if (users !== null && users !== undefined) {
-            users.push(userName);
-        } else {
-            users = [userName];
-        }
-        sessionStorage.setItem("users", users);
     };
 
     onUserLeft = (userName) => {
-        let users = sessionStorage.getItem("users");
-        if (users) {
-            users = Array.from(users);
-            if (!users.includes(userName)) {
-                return;
-            }
-        }
-    
         const userListItem = document.querySelector(".users_list");
         const usersItem = Array.from(userListItem.querySelectorAll('.users_list--user'));
         const userItem = usersItem.filter(item => item.innerHTML.includes(userName))[0];
         userListItem.removeChild(userItem);
-    
-        users = users.filter(i => i === userName);
-        sessionStorage.setItem("users", users);
     };
 
     getConnectedUsers = () => {
